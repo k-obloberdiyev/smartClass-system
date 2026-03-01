d4a026e6f4dc8741d66fc11b52ae3a231c26e173
k-obloberdiyev <kamol.obloberdiyev@gmail.com>
2026-03-01 11:44:20 +0500

    Now the client run forever, waiting for commands from the server(teacher).

--- test_socket(multiple_client_handling)/client.py
+++ test_socket(multiple_client_handling)/client.py
@@ -4,16 +4,22 @@ import os
 client_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client_sock.connect(('127.0.0.1', 5000))
 
-while True:
-    data = client_sock.recv(2048)
-    if not data:
-        break
-    
-    command = data.decode()
-    if command == 'LOCK': 
-        os.system("rundll32.exe user32.dll,LockWorkStation")
-    elif command == 'SHUTDOWN':
-        os.system("shutdown /s /t 0")
-    elif command.startswith('OPEN_IELTS'):
-        os.system("start iexplore.exe PATH_TO_WEBSITE")
+try:
+    while True:
+        data = client_sock.recv(2048)
+        if not data:
+            continue
+        
+        command = data.decode().strip()
+        print(f"Received command: {command}")
+
+        if command == 'LOCK': 
+            os.system("rundll32.exe user32.dll,LockWorkStation")
+        elif command == 'SHUTDOWN':
+            os.system("shutdown /s /t 0")
+        elif command.startswith('OPEN_IELTS'):
+            url = command.split(' ', 1)[1]
+            os.system("start iexplore.exe {url}")
+except KeyboardInterrupt:
+    client_sock.close()
     
