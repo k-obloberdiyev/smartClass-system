31c29988f235285f0defcc85001f86a265a89c95
k-obloberdiyev <kamol.obloberdiyev@gmail.com>
2026-03-02 12:33:40 +0500

    Adding main features(Shutting down and Locking computers)

---
+++ server.py
@@ -0,0 +1,45 @@
+import socket
+import threading
+
+clients = []
+clients_lock = threading.Lock()
+
+def server_input():
+    while True:
+        command = input("Enter command for students: ")
+        with clients_lock:
+            for client in clients:
+                try:
+                    client.sendall(command.encode())
+                except:
+                    clients.remove(client)
+
+def handle_client(client_sock, client_addr):
+    print("New Connection from", client_addr)
+    with clients_lock:
+        clients.append(client_sock)
+    try:
+        while True:
+            data = client_sock.recv(2048)
+            if not data:
+                break
+            print(f"Received from {client_addr}: {data.decode()}")
+    finally:
+        with clients_lock:
+            if client_sock in clients:
+                clients.remove(client_sock)
+        client_sock.close()
+        print(f"Connection closed {client_addr}")
+
+serv_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM, proto=0)
+serv_sock.bind(('127.0.0.1', 5000))
+serv_sock.listen(10)
+
+print("Server is listening...")
+
+threading.Thread(target=server_input, daemon=True).start()
+
+while True:
+    client_sock, client_addr = serv_sock.accept()
+    thread = threading.Thread(target=handle_client, args=(client_sock, client_addr))
+    thread.start()
